359fd871f27d4d85860ea89a0357d73302abcc12
audreycollette <audrey.collette@videotron.ca>
2018-12-05 09:30:40 -0500

    le tuto de angular, il fonctionne, mais je n'ai pas fini

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[]=[
+{id: 11 , name:'Mr. Nice'},
+{id:12 , name: 'Narco'},
+{id:13 , name:'Bombasto'},
+{id:14 ,name: 'celeritas'},
+{id: 15 , name: 'magneta'},
+{id: 16 , name: 'rubberman'},
+{id: 17 ,name: 'Dynama'},
+{id: 18 , name:'Dr iq'},
+{id:19 , name: 'magna'},
+{id:20 , name:'tornado'}
+];
